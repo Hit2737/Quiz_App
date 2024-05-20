270bab8645cfdb86f25d1cef328584dab7cb42e5
RuchitJagodara <ruchit.jagodara@iitgn.ac.in>
2024-05-20 18:27:35 +0530

    Added working register and login files and functions and declared the location

--- quiz_app/__init__.py
+++ quiz_app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 
 def create_app(test_config=None):
     # Create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, template_folder='../templates')
     app.config.from_mapping(
         SECRET_KEY='dev', 
         DATABASE=os.path.join(app.instance_path, 'quiz_app.sqlite')
