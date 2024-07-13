0d907e122613f8e521cde4fcd94f685649574282
Hit2737 <hitesh.kumar@iitgn.ac.in>
2024-07-13 17:08:04 +0530

    minor fixes in auth.py

--- quiz_app/auth.py
+++ quiz_app/auth.py
@@ -70,7 +70,7 @@ def register():
                 db.commit()
                 return redirect(url_for("auth.login_student"))  
             except db.IntegrityError:
-                error = f"User {username} is already registered."
+                error = f"Username or Email is already registered."
         flash(error)
     return render_template('auth/register.html')
 
